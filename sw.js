102823cfc93f2adab8380863f95f4a6391d7881c
n39922453yy-netize <39922453yy@gmail.com>
2026-04-18 20:18:43 +0900

    cache update

--- sw.js
+++ sw.js
@@ -1,23 +1,25 @@
+const CACHE_NAME = "cat-health-app-ultimate-v2";
 const CACHE_NAME = "cat-health-app-ultimate-v1";
 const urlsToCache = [
 "./",
 "./index.html",
-"./manifest.json"
+"./manifest.json",
+"./sw.js"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
 event.waitUntil(
-caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
 );
 self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
 event.waitUntil(
-caches.keys().then((keys) =>
+caches.keys().then(keys =>
 Promise.all(
-keys.map((key) => {
-if (key !== CACHE_NAME) {
+keys.map(key => {
+if(key !== CACHE_NAME){
 return caches.delete(key);
 }
 })
@@ -27,9 +29,9 @@ return caches.delete(key);
 self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
 event.respondWith(
-caches.match(event.request).then((cachedResponse) => {
+caches.match(event.request).then(cachedResponse => {
 return cachedResponse || fetch(event.request).catch(() => caches.match("./index.html"));
 })
 );
